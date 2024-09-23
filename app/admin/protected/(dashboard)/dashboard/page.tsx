d1dafd114cd11dd56df4ddd081f31df0f2dd3048
Adhham Safwan <msmadhhamg@gmail.com>
2024-09-24 02:03:37 +0530

    chore: Update IslamicPattern component in dashboard pages
    The code changes in this commit update the IslamicPattern component in the dashboard pages. The component is now extracted into its own file and imported into both the dashboard/page.tsx and dashboard/layout.tsx files. This improves code organization and reusability.

--- app/admin/protected/(dashboard)/dashboard/page.tsx
+++ app/admin/protected/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,21 @@
 import DashboardMain from "@/components/dashboard/DashboardMain";
 
+
+function IslamicPattern() {
+    return (
+        <div className="absolute inset-0 opacity-25 pointer-events-none">
+            <div className="w-full h-full bg-repeat" style={{
+                backgroundImage: `url("/bg-pattern.svg")`
+            }} ></div>
+        </div>
+    )
+}
+
 export default function Dashboard() {
 
     return (
         <div className="min-h-screen bg-emerald-100">
+            <IslamicPattern />
             <DashboardMain />
         </div>
     )
--- app/admin/protected/(dashboard)/layout.tsx
+++ app/admin/protected/(dashboard)/layout.tsx
@@ -1,15 +1,7 @@
 import DashboardHeader from "@/components/dashboard/DashboardHeader";
 import DashboardNavigation from "@/components/dashboard/DashboardNavigation";
 
-function IslamicPattern() {
-    return (
-        <div className="absolute inset-0 opacity-25 pointer-events-none">
-            <div className="w-full h-full bg-repeat" style={{
-                backgroundImage: `url("/bg-pattern.svg")`
-            }} ></div>
-        </div>
-    )
-}
+
 
 export default function RootLayout({
     children,
@@ -19,7 +11,7 @@ export default function RootLayout({
     return (
 
         <>
-            <IslamicPattern />
+
             <DashboardHeader />
             <DashboardNavigation />
             {children}
