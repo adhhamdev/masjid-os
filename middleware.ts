7cf5282b00dae1bd70579bcb150709ad945fc0f4
Adhham Safwan <msmadhhamg@gmail.com>
2024-09-25 19:47:13 +0530

    refactor: Update matcher in middleware.ts
    This commit updates the matcher in the middleware.ts file to include additional patterns for excluding certain routes. The new patterns exclude the "/api" route and any routes that consist of only numbers. This improves the routing logic and ensures that the middleware is applied to the desired routes.

--- middleware.ts
+++ middleware.ts
@@ -18,5 +18,8 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|\\/\\d+$).*)"]
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!^[^/]+$).*)'
+  ]
 }
