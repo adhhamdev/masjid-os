293e9b9764c2cc901b78a36739b00236805e37f3
Adhham Safwan <msmadhhamg@gmail.com>
2024-09-29 17:17:41 +0530

    refactor: Update middleware to handle sign-in redirects and authentication errors
    This commit updates the middleware in the `middleware.ts` file to handle sign-in redirects and authentication errors. It improves the logic for redirecting users to the sign-in page if they are not authenticated and trying to access protected routes. Additionally, it adds error handling for authentication errors and redirects users to the sign-in page in case of an error. These changes enhance the security and user experience of the application.

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,31 @@
-import { updateSession } from '@/utils/supabase/middleware'
-import { createClient } from '@/utils/supabase/server'
-import { NextResponse, type NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { createClient } from './utils/supabase/server';
 
-export async function middleware(request: NextRequest) {
-  // First, update the session to refresh tokens if necessary
-  const response = await updateSession(request)
-
-  // If updateSession returns a response, it means a redirect occurred (e.g., to login page)
-  if (response) return response
-
-  const supabase = createClient()
+export async function middleware(req: NextRequest) {
+  const supabase = createClient();
   
   try {
     const { data: { user } } = await supabase.auth.getUser();
 
-    if (!user && !request.nextUrl.pathname.startsWith('/admin/sign-in') && request.nextUrl.pathname !== '/') {
-      return NextResponse.redirect(new URL('/admin/sign-in', request.url));
+    if (!user && !req.nextUrl.pathname.startsWith('/admin/sign-in') && req.nextUrl.pathname !== '/') {
+      return NextResponse.redirect(new URL('/admin/sign-in', req.url));
     }
 
-    if (user && request.nextUrl.pathname === '/admin/sign-in') {
-      return NextResponse.redirect(new URL('/admin/protected/dashboard', request.url));
+    if (user && req.nextUrl.pathname === '/admin/sign-in') {
+      return NextResponse.redirect(new URL('/admin/protected/dashboard', req.url));
     }
 
     return NextResponse.next();
   } catch (error) {
     console.error('Authentication error:', error);
     // If there's an authentication error, redirect to sign-in
-    return NextResponse.redirect(new URL('/admin/sign-in', request.url));
+    return NextResponse.redirect(new URL('/admin/sign-in', req.url));
   }
 }
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|clock/.*|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 }
