dfab5e224ae3a338d4bcb3671e5dbdaf3d3501cf
Adhham Safwan <msmadhhamg@gmail.com>
2024-09-29 17:49:30 +0530

    refactor: Update middleware to handle sign-in redirects and authentication errors

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,23 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { updateSession } from './utils/supabase/middleware';
+import { createClient } from './utils/supabase/server';
 
 export async function middleware(req: NextRequest) {
   await updateSession(req);
-    return NextResponse.next();
+
+  const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+
+  if (!user && !req.nextUrl.pathname.startsWith('/admin/sign-in') && req.nextUrl.pathname !== '/') {
+    return NextResponse.redirect(new URL('/admin/sign-in', req.url));
+  }
+
+  if (user && req.nextUrl.pathname === '/admin/sign-in') {
+    return NextResponse.redirect(new URL('/admin/protected/dashboard', req.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
